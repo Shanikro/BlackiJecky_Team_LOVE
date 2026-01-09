5dd8fe8bf4705f7b4dc81ce1cfe24ccfee3b0b57
Shani Kroitoro <shanikro29@gmail.com>
2026-01-09 19:10:22 +0200

    fix

--- server/BlackJeckServer.py
+++ server/BlackJeckServer.py
@@ -92,11 +92,13 @@ def main():
         print("\nShutting down server...")
         signal_handler(None, None)
 
-def play_game(conn: socket.socket, rounds: int, name: str):
+def play_game(conn: socket.socket, rounds: int, player_name: str):
+
+    print(f"Starting {rounds} rounds with player {player_name}")
 
     for round_num in range(1, rounds + 1):
-        print(f"Round {round_num}/{rounds}")
         print(f"{'='*50}")
+        print(f"Round {round_num}/{rounds} - {player_name}")
         
         game = BlackjackGame()
 
@@ -107,7 +109,7 @@ def play_game(conn: socket.socket, rounds: int, name: str):
         conn.sendall(encode_server_payload(game.result, second_card_player.rank, second_card_player.suit))
 
         if game.result == game.ROUND_RESULT.DEALER_WINS:
-            decide_winner(game.result)
+            decide_winner(game.result, player_name)
             continue # Dealer wins, no need to continue the round
 
         first_card_dealer = game.dealer_hit()
@@ -124,7 +126,7 @@ def play_game(conn: socket.socket, rounds: int, name: str):
                 conn.sendall(encode_server_payload(game.result, card.rank, card.suit))
 
                 if game.player_hand.is_bust():
-                    decide_winner(game.result)
+                    decide_winner(game.result, player_name)
                     break 
 
             elif decision == "STAND":
@@ -135,15 +137,15 @@ def play_game(conn: socket.socket, rounds: int, name: str):
                     card = game.dealer_hit()
                     conn.sendall(encode_server_payload(game.result, card.rank, card.suit))
                 
-                decide_winner(game.result)
+                decide_winner(game.result, player_name)
 
                 break # Dealer plays, decide winner
 
-def decide_winner(result: int):
+def decide_winner(result: int, player_name: str):
     if result == BlackjackGame.ROUND_RESULT.DEALER_WINS:
         print("Dealer wins round")
     elif result == BlackjackGame.ROUND_RESULT.PLAYER_WINS:
-        print("Player wins round")
+        print(f"Player {player_name} wins round")
     else:
         print("Tie")
 
